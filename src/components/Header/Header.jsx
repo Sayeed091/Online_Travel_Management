ddc1572837c3127706ebbfedabec7aed8c202e7c
SayeedHossain <sayeed.hossain360@gmail.com>
2023-07-08 12:58:30 +0600

    header update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Container, Row, Button } from 'reactstrap';
 import { NavLink, Link, } from 'react-router-dom';
+import logo from '../../assets/images/logo.png'
+
+
 
 const nav__links = [
     {
@@ -26,6 +29,45 @@ const Header = () => {
                 <div className="nav__wrapper d-flex align-items-center
                 justify-content-between">
 
+                    {/*----------- logo start ------------*/}
+                    <div className='logo'>
+                        <img src={logo} alt="" />
+                    </div>
+                    {/*----------- logo end -------------*/}
+
+                    {/*----------- menu start ------------*/}
+                    <div className='navigation'>
+                        <ul className='menu d-flex align-items-center  gap-5'>
+                            {
+                                nav__links.map((item, index) => (
+
+                                    <li className="nav__item" key={index}>
+                                        <NavLink to={item.path}>
+                                            {item.display}
+
+                                        </NavLink>
+                                    </li>
+                                ))}
+                        </ul>
+                    </div>
+                    {/*----------- menu end ------------*/}
+
+                    <div className='nav__right d-flex align-items-center gap-4'>
+                        <div className='nav__btns d-flex align-items-center gap-4'>
+                            <Button className='btn secondary__btn'>
+                                <Link to='/login'>Login
+                                </Link></Button>
+
+                            <Button className='btn primary__btn'>
+                                <Link to='/register'>Register</Link>
+                            </Button>
+                        </div>
+                        <span className='mobile__menu'>
+                            <i className="ri-menu-2-line"></i>
+
+                        </span>
+
+                    </div>
                 </div>
             </Row>
         </Container>
